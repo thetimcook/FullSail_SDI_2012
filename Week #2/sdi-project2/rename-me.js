452ad14d240e08412326d3ee5f4acac093ba33ae
Tim Cook <redeemingdesign@gmail.com>
2012-07-11 13:15:20 -0500

    Added Procedure

--- Week #2/sdi-project2/rename-me.js
+++ Week #2/sdi-project2/rename-me.js
@@ -1,1 +1,20 @@
 alert("JavaScript works!");
+/*		Tim Cook
+		SDI 1207
+		Project 2
+		Functions and Loops
+*/
+
+
+// Variables
+var passenger = "Tim Cook";
+	numberOfBags = 2;
+	airports = ["Springfield-Branson Airport", "Hartsfield-Jackson Atlanta Airport", "Orlando Sanford Airport"];
+
+// Procedure
+if (passenger = "Tim Cook") {
+	console.log(passenger + " will be flying to " + airport[2] + " from " + airport[0]);
+	} else { 
+	console.log("Tim Cook will not be traveling today");
+};
+
