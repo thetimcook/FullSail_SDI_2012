0c87d5941dd383382401dfb46b981dfc21103583
Tim Cook <redeemingdesign@gmail.com>
2012-07-11 13:44:35 -0500

    Boolean Function done

--- Week #2/sdi-project2/rename-me.js
+++ Week #2/sdi-project2/rename-me.js
@@ -1,4 +1,4 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
 /*		Tim Cook
 		SDI 1207
 		Project 2
@@ -7,14 +7,33 @@ alert("JavaScript works!");
 
 
 // Variables
-var passenger = "Tim Cook";
-	numberOfBags = 2;
+var passenger = "Tim Cook",
+	numberOfBags = 2,
 	airports = ["Springfield-Branson Airport", "Hartsfield-Jackson Atlanta Airport", "Orlando Sanford Airport"];
 
 // Procedure
-if (passenger = "Tim Cook") {
-	console.log(passenger + " will be flying to " + airport[2] + " from " + airport[0]);
+if (passenger === "Tim Cook") {
+	console.log(passenger + " will be flying to " + airports[2] + " from " + airports[0]);
 	} else { 
 	console.log("Tim Cook will not be traveling today");
-};
+}; // End of Procedure
 
+// Boolean Function
+var planeDelayed = function (onTime, Delayed) {
+	if ((onTime === true) + (Delayed === false)) {
+		return"It is " + onTime + " that the plane is going to be on time today.";
+	} else {
+		return"It is " + Delayed + " that the plane is going to be on time.";
+	};
+};// End Boolean Function
+
+// Number Function
+
+
+
+
+
+
+
+
+console.log(planeDelayed(true, false));
