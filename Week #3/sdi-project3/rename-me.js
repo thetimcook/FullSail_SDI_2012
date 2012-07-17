5b5c5a270dbb26070b0bde312efccaeb40289146
Tim Cook <redeemingdesign@gmail.com>
2012-07-17 11:27:16 -0500

    added JSON data

--- Week #3/sdi-project3/rename-me.js
+++ Week #3/sdi-project3/rename-me.js
@@ -1,1 +1,17 @@
-alert("JavaScript works!");
+// alert("JavaScript works!");
+/*		Tim Cook
+		SDI 1207
+		Project 3
+		Custom Flowchart and JSON Data
+*/
+
+// Global Variables
+var passengers = ["Tim Cook", "Dale Cook", "Tammy Cook"]
+	
+
+
+
+
+
+
+
