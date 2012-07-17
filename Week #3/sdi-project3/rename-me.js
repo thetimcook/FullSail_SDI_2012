3a8749e9f6a77358be29159a4950f9243fb3d053
Tim Cook <redeemingdesign@gmail.com>
2012-07-17 13:32:14 -0500

    added procedure

--- Week #3/sdi-project3/rename-me.js
+++ Week #3/sdi-project3/rename-me.js
@@ -6,12 +6,22 @@
 */
 
 // Global Variables
-var passengers = ["Tim Cook", "Dale Cook", "Tammy Cook"]
+var passengers = ["Tim Cook", "Dale Cook", "Tammy Cook"];
 	
 
 
 
+// Procedure
+var trip = function (passengers) {
+	if (passengers.length > 0) {
+		while (passengers.length > 0) {
+		console.log(passengers[passengers.length - 1] + " gets ready for the flight.");
+		passengers.length --;
+		}
+	} else { 
+		console.log("No one is flying today.");
+	};
+};// End of Procedure
 
 
-
-
+console.log(trip(passengers));
