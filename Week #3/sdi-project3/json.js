bc53b00dd5a5a4349aa18d27f9289ee7d8596af6
Tim Cook <redeemingdesign@gmail.com>
2012-07-18 13:45:42 -0500

    added json data

--- Week #3/sdi-project3/json.js
+++ Week #3/sdi-project3/json.js
@@ -5,37 +5,67 @@
 */
 
 
-var myFlights = {
-	"001": {
-		flight: "675",
-		terminal: "A2",
-		onTime: true,
-		departure: "Springfield-Branson Airport",
-		departureTime: 8,
-		arrival: "Dallas-Fort Worth Airport",
-		arrivalTime: 10
-	},
-	"002": {
-		flight: "676",
-		terminal: "B6",
-		onTime: true,
-		departure: "Dallas-Fort Worth Airport",
-		departureTime: 12,
-		arrival: "Hartsfield-Jackson Atlanta Airport",
-		arrivalTime: 13.5
-	},
-	"003": {
-		flight: "367",
-		terminal: "A12",
-		onTime: true,
-		departure: "Hartsfield-Jackson Atlanta Airport",
-		departureTime: 14.5,
-		arrival: "Orlando Sanford Airport",
-		arrivalTime: 16
+var json = {
+	"myFlights": {
+		"001": {
+			"flight": "675",
+			"terminal": "A2",
+			"onTime": true,
+			"departure": "Springfield-Branson Airport",
+			"departureTime": 8,
+			"arrival": "Dallas-Fort Worth Airport",
+			"arrivalTime": 10
+		},
+		"002": {
+			"flight": "676",
+			"terminal": "B6",
+			"onTime": true,
+			"departure": "Dallas-Fort Worth Airport",
+			"departureTime": 12,
+			"arrival": "Hartsfield-Jackson Atlanta Airport",
+			"arrivalTime": 13.5
+		},
+		"003": {
+			"flight": "367",
+			"terminal": "A12",
+			"onTime": true,
+			"departure": "Hartsfield-Jackson Atlanta Airport",
+			"departureTime": 14.5,
+			"arrival": "Orlando Sanford Airport",
+			"arrivalTime": 16
+		}
 	}
 };
 
-console.log(myFlights);
-
-
-
+var json2 = {
+	"myFlights": [
+		{
+			"flight": "675",
+			"terminal": "A2",
+			"onTime": true,
+			"departure": "Springfield-Branson Airport",
+			"departureTime": 8,
+			"arrival": "Dallas-Fort Worth Airport",
+			"arrivalTime": 10
+		},
+		{
+			"flight": "676",
+			"terminal": "B6",
+			"onTime": true,
+			"departure": "Dallas-Fort Worth Airport",
+			"departureTime": 12,
+			"arrival": "Hartsfield-Jackson Atlanta Airport",
+			"arrivalTime": 13.5
+		},
+		{
+			"flight": "367",
+			"terminal": "A12",
+			"onTime": true,
+			"departure": "Hartsfield-Jackson Atlanta Airport",
+			"departureTime": 14.5,
+			"arrival": "Orlando Sanford Airport",
+			"arrivalTime": 16
+		}
+	
+	]
+};
--- Week #3/sdi-project3/rename-me.js
+++ Week #3/sdi-project3/rename-me.js
@@ -7,20 +7,35 @@
 
 // Global Variables
 var passengers = ["Tim Cook", "Dale Cook", "Tammy Cook"];
-	nubmerOfpassengers = passengers.length
-
 
 
 // Procedure
 var trip = function (passengers) {
-	if (nubmerOfpassengers > 0) {
-		while (passengers.length > 0) {
-		console.log(passengers[passengers.length - 1] + " gets ready for the flight.");
-		passengers.length --;
+	var nubmerOfPassengers = passengers.length
+	if (nubmerOfPassengers > 0) {
+		while (nubmerOfPassengers > 0) {
+		console.log(passengers[nubmerOfPassengers - 1] + " gets ready for the flight.");
+		nubmerOfPassengers --;
 		}
 	} else { 
 		console.log("No one is flying today.");
 	};
 };// End of Procedure
 
+
+
+// Array Function
+var checkFlightStatus = function (json) {
+	for (var i = 0; i < json.myFlights.length; i++){
+		var flight = json.myFlights[i];
+		console.log("We will be fling out of " + flight.departure + " at " + flight.departureTime + ":00am, and arriving at " + flight.arrival + " at " + flight.arrivalTime + ":00.");
+	};
+};
+
+
+// Main Code
+
 trip(passengers);
+
+checkFlightStatus(json2);
+
